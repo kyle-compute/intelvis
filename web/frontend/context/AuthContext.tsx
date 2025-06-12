3dcd5d7f62a43b8b5bdcabff54b6be5f436f7607
kel <kyle@intelvis.ai>
2025-06-11 20:42:55 -0700

    idk what it did

--- web/frontend/context/AuthContext.tsx
+++ web/frontend/context/AuthContext.tsx
@@ -24,7 +24,7 @@ interface AuthContextType {
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://intelvis.ai';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null)
